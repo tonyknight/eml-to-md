d5f7ed8af0a962748ddd3ff9fc8d16594823f1ab
Tony Knight <tonyknight@gmail.com>
2023-03-10 21:01:37 -0800

    (2023-03-10) Modifications

--- eml-to-md.py
+++ eml-to-md.py
@@ -3,8 +3,8 @@ import email
 import markdown
 import shutil
 from datetime import datetime
-from dateutil import parser
-from dateutil import tz
+from dateutil import parser, tz
+from uuid import uuid4
 
 # Folder path containing .eml files
 folder_path = '/Users/knight/Desktop/Imported/Sent Items.mbox/eml'
@@ -21,6 +21,9 @@ if not os.path.exists(done_folder):
 
 # Function to remove illegal characters from filename
 def clean_filename(filename):
+    """
+    Removes illegal characters from a filename and returns the cleaned filename.
+    """
     cleaned = ''.join(c if c.isalnum() or c in [' ', '.', '_'] else '_' for c in filename)
     return cleaned.strip()
 
@@ -43,12 +46,13 @@ for filename in os.listdir(folder_path):
                     msg = email.message_from_string(contents)
 
             # Extract date, sender, and subject from email message
-            date_str = msg['Date']
-            sender = msg['From']
-            subject = msg['Subject']
+            date_str = msg.get('Date', '')
+            sender = msg.get('From', '')
+            subject = msg.get('Subject', '')
 
             # Convert email body to markdown
             body = ''
+            attachments = []
             for part in msg.walk():
                 if part.get_content_type() == 'text/plain':
                     body += part.get_payload()
@@ -58,19 +62,25 @@ for filename in os.listdir(folder_path):
                     # Download attachment and create link
                     filename = part.get_filename()
                     if filename:
-                        filepath = os.path.join(attachments_folder, filename)
+                        # Generate a unique filename for the attachment
+                        new_filename = str(uuid4()) + '_' + clean_filename(filename)
+                        filepath = os.path.join(attachments_folder, new_filename)
                         with open(filepath, 'wb') as f:
                             f.write(part.get_payload(decode=True))
-                        link = f"[{filename}]({filename})"
-                        body += f"\n\n{link}"
+                        link = f"[{filename}]({new_filename})"
+                        attachments.append(link)
 
-            body = markdown.markdown(body)
+            # Add headers to the beginning of the Markdown output
+            body = f"From: {sender}\nSubject: {subject}\nDate: {date_str}\n\n{body}"
+            if attachments:
+                body += '\n\nAttachments:\n\n' + '\n'.join(attachments)
 
             # Parse date string into datetime object
             try:
                 date = parser.parse(date_str)
                 date = date.astimezone(tz.tzlocal()).replace(tzinfo=None)
             except (ValueError, TypeError):
+                # If parsing fails, write error message to log file and move on to next file
                 with open('error.log', 'a') as logfile:
                     logfile.write(f"Error parsing date for file {filename}\n")
                 continue
@@ -78,18 +88,25 @@ for filename in os.listdir(folder_path):
             # Format date as (YYYY-MM-DD) HH:MM:SS
             date_str = f"({date.strftime('%Y-%m-%d')}) {date.strftime('%H-%M-%S')}"
 
-            # Clean up filename
-            sender_cleaned = clean_filename(sender)
-            subject_cleaned = clean_filename(subject)
-            new_filename = f"{date_str} - {sender_cleaned} - {subject_cleaned}.md"
 
-            # Write markdown file
-            with open(os.path.join(folder_path, new_filename), 'w') as f:
-                f.write(body)
 
-            # Move processed .eml file to 'done' folder
-            shutil.move(os.path.join(folder_path, filename), os.path.join(done_folder, filename))
+               # Clean up filename
+               sender_cleaned = clean_filename(sender)
+               subject_cleaned = clean_filename(subject)
+               new_filename = f"{date_str} - {sender_cleaned} - {subject_cleaned}.md"
 
-        except Exception as e:
-            with open('error.log', 'a') as logfile:
-                logfile.write(f"Error processing file {filename}: {e}\n")
+               # Write markdown file
+               output_folder = os.path.join(folder_path, 'output')
+               if not os.path.exists(output_folder):
+                   os.makedirs(output_folder)
+               output_path = os.path.join(output_folder, new_filename)
+               with open(output_path, 'w') as f:
+                   f.write(body)
+
+               # Move processed .eml file to 'done' folder
+               shutil.move(os.path.join(folder_path, filename), os.path.join(done_folder, filename))
+
+           except Exception as e:
+               with open('error.log', 'a') as logfile:
+                   logfile.write(f"Error processing file {filename}: {e}\n")
+           
